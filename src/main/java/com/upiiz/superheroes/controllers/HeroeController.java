7b00dfa4eb9b0a2363708d47c57bc11f3af722cf
Alonso <alonsolopezdominguez3@gmail.com>
2024-12-17 07:31:33 -0600

    agregado el CORS para poder hacer peticiones AJAX

--- src/main/java/com/upiiz/superheroes/controllers/HeroeController.java
+++ src/main/java/com/upiiz/superheroes/controllers/HeroeController.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RequestMapping("/api/v1/heroes")
 @Tag(
         name = "Superheroes"
